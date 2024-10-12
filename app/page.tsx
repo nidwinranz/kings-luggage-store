786d2ccede078c2b66cdf978c954bd621736e006
nidwinranz <nidwin.ranze@gmail.com>
2024-10-12 18:22:34 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,239 @@
-import Image from "next/image";
+
+'use client';
+import NavBar from '../components/NavBar';
+import Image from 'next/image'; 
+import picture1 from '../public/pic1.jpg';
+import {Button} from "@nextui-org/react";
+import picture2 from '../public/pic2.jpg';
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex flex-col min-h-screen p-8 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <NavBar />
+      <section>
+      <div className="flex flex-col md:flex-row items-center justify-center mt-4 md:mt-12 md:ml-4 z-" style={{ marginTop: '150px' }}> {/* Adjust this value based on your navbar height */}
+        
+      <Image 
+  src={picture1} 
+  alt="Description of the image" 
+  width={700} 
+  height={900} 
+  className="rounded-xl object-cover mb-4 md:mb-0 md:w-1/2 shadow-xl" 
+/>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="flex flex-col items-center justify-center mr-6 mt-4 md:mt-0 md:ml-4"> 
+          <h1 className="text-4xl md:text-6xl font-bold mb-2 text-center">London’s Best Luggage Storage Service</h1>
+          <p className="text-2xl mb-2 text-center mt-4 font-beautiful ">
+            Please Book Online For Cheaper Price, Insurance & Security Purposes. Explore our features, learn about our services, and discover what we have to offer.
+          </p>
+          <p className="text-xl mb-2 text-center mt-6 font-serif">
+            Online Price £4/bag
+          </p>
+          <p className="text-xl mb-2 text-center font-serif">
+            In store price £5/bag
+          </p>
+          <p className="text-xl mb-2 text-center font-bold font-beautiful mt-6">
+            Cancellation Is Free Before Booking Time Start. For Any inquiries Please Call, text, or Email Us (24/7) Customer Support Available.         
+    
+          </p>
+          <Button className="bg-green-900 text-white py-2 px-4 rounded-xl hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-50 transition-all shadow-lg hover:shadow-green-500/50 flex items-center mt-4">
+  BOOK NOW
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    className="ml-2 w-5 h-5"
+  >
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 7l5 5m0 0l-5 5m5-5H6" />
+  </svg>
+</Button>
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div className="mt-32"> 
+  
+      </div>
+
+      </section>
+       
+<section>
+<div className="flex flex-col md:flex-row justify-between mt-6">
+  <div className="text-4xl mb-2 font-bold text-center md:ml-[40px]">
+    <p>London’s Best Luggage Storage Service</p>
+    
+    {/* 5 Star SVG */}
+    <div className="flex justify-center my-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="w-8 h-8 text-yellow-500" // Adjust size and color as needed
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path d="M10 15.27L16.18 18l-1.64-7.03L18 8.24l-7.19-.61L10 1 9.19 7.63 2 8.24l5.46 2.73L5.82 18z" />
+      </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="w-8 h-8 text-yellow-500"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path d="M10 15.27L16.18 18l-1.64-7.03L18 8.24l-7.19-.61L10 1 9.19 7.63 2 8.24l5.46 2.73L5.82 18z" />
+      </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="w-8 h-8 text-yellow-500"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path d="M10 15.27L16.18 18l-1.64-7.03L18 8.24l-7.19-.61L10 1 9.19 7.63 2 8.24l5.46 2.73L5.82 18z" />
+      </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="w-8 h-8 text-yellow-500"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path d="M10 15.27L16.18 18l-1.64-7.03L18 8.24l-7.19-.61L10 1 9.19 7.63 2 8.24l5.46 2.73L5.82 18z" />
+      </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="w-8 h-8 text-yellow-500"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path d="M10 15.27L16.18 18l-1.64-7.03L18 8.24l-7.19-.61L10 1 9.19 7.63 2 8.24l5.46 2.73L5.82 18z" />
+      </svg>
+    </div>
+    <br></br>
+
+    <p>1400+ Google Reviews</p>
+  </div>
+
+
+  <div className="text-center bg-green-900 text-white p-2 border-spacing-5 border-white rounded-3xl mt-4 md:mt-0 md:mr-[80px] shadow-2xl">
+  <h2 className="text-5xl md:text-6xl mb-2 font-bold">Call us now</h2>
+  <p className="text-4xl md:text-7xl mb-2 font-bold">075 3354 4428</p>
+</div>
+
+</div>
+</section>
+
+<section>
+  <br />
+  <div className="flex flex-col md:flex-row items-center justify-end mt-4 md:mt-12 md:mr-22 z-0" style={{ marginTop: '30px' }}>
+
+  <div className="text-center bg-green-900 text-white p-20 border-spacing-5 border-white rounded-3xl mt-4 mb-12 md:mt-10 md:mr-[80px] ">
+  <h3 className="text-2xl md:text-5xl md:mt-2 mb-12 font-bold">Kings Cross Luggage Storage</h3>
+<p className="text-xl md:text-2xl mb-8 md:mt-6 font-bold">Open 7 Days 07:00 – 23:59</p>
+<p className="text-xl md:text-2xl mb-8 md:mt-6 font-bold">345 Grays Inn Road, Kings Cross,
+London, WC1X 8PX</p>
+<p className="text-xl md:text-xl  mb-6 font-beautiful ">Leave your luggage safely in our store and collect your belongings at the time that suits you. We are opposite
+   the Kings cross station platform 01 & 02.All your luggages are insured. Pickup and drop off anytime between opening hours.</p>
+
+    <div className="flex justify-center mt-4">
+      <Button className="bg-white text-green-600 py-6 px-8 rounded-2xl hover:bg-green-200 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-50 transition-all shadow-lg hover:shadow-green-500/50 flex items-center">
+        BOOK NOW
+      </Button>
+    </div>
+
+  </div>
+    <Image 
+      src={picture2} 
+      alt="Description of the image" 
+      width={450}  
+      height={300} 
+      className="rounded-xl object-cover mb-4 md:mb-0 md:w-1/2 shadow-xl md:mr-[80px]"
+      
+    />
+      
+  </div>
+</section>
+
+<div className=" left-0 right-0">
+  <section className=" absolute left-0 right-0">
+    <div className="w-full flex justify-center bg-green-900 text-white mt-4 shadow-2xl p-4">
+      <h2 className="text-2xl md:text-4xl mb-2 font-bold text-center whitespace-normal">
+        +4475 3354 4428<br />
+        info@kingscrossluggage.co.uk
+      </h2>
+    </div>
+  </section>
+</div>
+
+
+
+<section>
+  <div className="flex flex-col md:flex-row items-start justify-between mt-4 md:mt-12 md:ml-10 md:mr-22 z-0" style={{ marginTop: '180px' }}>
+    
+  
+    <div
+      style={{
+        maxWidth: '100%',
+        listStyle: 'none',
+        transition: 'none',
+        overflow: 'hidden',
+        width: '800px',
+        height: '600px'
+        
+      }}
+    >
+      <div id="google-maps-canvas" style={{ height: '100%', width: '100%', maxWidth: '100%' }}>
+        <iframe
+          style={{ height: '100%', width: '100%', border: 0 }}
+          frameBorder="0"
+          src="https://www.google.com/maps/embed/v1/place?q=Kings+Cross+Luggage+Storage&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
+
+    <div className="md:ml-10 mt-8 md:mt-0 md:w-1/2">
+      <h2 className="text-8xl font-bold mb-4 text-center text-green-900">£4.00</h2>
+      <p className="text-xl mb-4 text-center"> PER BAG PER DAY
+      </p>
+
+      <hr className="h-1 bg-green-800 border-none" />
+
+      <ul className="list-disc list-inside text-xl mb-4 line-height-8 mt-4">
+  <li className="mb-2">
+    We are the cheapest Luggage storage in London
+  </li>
+  <li className="mb-2">
+     We have 1400+ Google 5 star reviews
+  </li>
+  <li className="mb-2">
+  24/7 CCTV and intruder alarms to keep your luggage safe
+  </li>
+  <li className="mb-2">
+  Open everyday from 7.00am until midnight
+  </li>
+  <li className="mb-2">
+  We offer long and short term storage options
+  </li>
+
+
+</ul>
+
+
+    </div>
+    
+    <style jsx>{`
+      #google-maps-canvas img {
+        max-width: none !important;
+        background: none !important;
+        font-size: inherit;
+        font-weight: inherit;
+      }
+    `}</style>
+  </div>
+</section>
+
+
+
+
+
     </div>
   );
 }
---
+++ components/NavBar.jsx
@@ -0,0 +1,266 @@
+"use client";
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
+
+function NavBar() {
+  const [navbar, setNavbar] = useState(false);
+
+  return (
+    <div>
+      <div className="flex flex-col md:flex-row justify-between items-center px-4 md:px-12 lg:flex-row xl:px-68 bg-white text-green-900 py-2 text-center fixed top-0 left-0 right-0 z-10 md:mt-4 md:top-[-20px] font-bold font-mono">
+  <div className="flex items-center">
+  <svg
+    className="w-6 h-6 mr-2"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    version="1.1"
+    width="256"
+    height="256"
+    viewBox="0 0 256 256"
+    xmlSpace="preserve"
+  >
+    <g
+      style={{
+        stroke: 'none',
+        strokeWidth: 0,
+        strokeDasharray: 'none',
+        strokeLinecap: 'butt',
+        strokeLinejoin: 'miter',
+        strokeMiterlimit: 10,
+        fill: 'none',
+        fillRule: 'nonzero',
+        opacity: 1,
+      }}
+      transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)"
+    >
+      <path
+        d="M 45 90 c -1.062 0 -2.043 -0.561 -2.583 -1.475 l -4.471 -7.563 c -9.222 -15.591 -17.933 -30.317 -20.893 -36.258 c -2.086 -4.277 -3.138 -8.852 -3.138 -13.62 C 13.916 13.944 27.86 0 45 0 c 17.141 0 31.085 13.944 31.085 31.084 c 0 4.764 -1.051 9.339 -3.124 13.596 c -0.021 0.042 -0.042 0.083 -0.063 0.124 c -3.007 6.005 -11.672 20.654 -20.843 36.159 l -4.472 7.563 C 47.044 89.439 46.062 90 45 90 z M 45 6 C 31.168 6 19.916 17.253 19.916 31.084 c 0 3.848 0.847 7.539 2.518 10.969 c 2.852 5.721 11.909 21.033 20.667 35.839 L 45 81.104 l 1.89 -3.196 c 8.763 -14.813 17.823 -30.131 20.687 -35.879 c 0.012 -0.022 0.023 -0.045 0.035 -0.067 c 1.642 -3.406 2.474 -7.065 2.474 -10.877 C 70.085 17.253 58.832 6 45 6 z"
+        style={{
+          stroke: 'none',
+          strokeWidth: 1,
+          strokeDasharray: 'none',
+          strokeLinecap: 'butt',
+          strokeLinejoin: 'miter',
+          strokeMiterlimit: 10,
+          fill: 'black',
+          fillRule: 'nonzero',
+          opacity: 1,
+        }}
+        transform="matrix(1 0 0 1 0 0)"
+        strokeLinecap="round"
+      />
+      <path
+        d="M 45 44.597 c -8.076 0 -14.646 -6.57 -14.646 -14.646 S 36.924 15.306 45 15.306 c 8.075 0 14.646 6.57 14.646 14.646 S 53.075 44.597 45 44.597 z M 45 21.306 c -4.767 0 -8.646 3.878 -8.646 8.646 s 3.878 8.646 8.646 8.646 c 4.768 0 8.646 -3.878 8.646 -8.646 S 49.768 21.306 45 21.306 z"
+        style={{
+          stroke: 'none',
+          strokeWidth: 1,
+          strokeDasharray: 'none',
+          strokeLinecap: 'butt',
+          strokeLinejoin: 'miter',
+          strokeMiterlimit: 10,
+          fill: 'rgb(0,0,0)',
+          fillRule: 'nonzero',
+          opacity: 1,
+        }}
+        transform="matrix(1 0 0 1 0 0)"
+        strokeLinecap="round"
+      />
+    </g>
+  </svg>
+
+
+    <p className="text-medium md:text-xl lg:text-xl">345 Grays Inn Road, Kings Cross, London, WC1X 8PX</p>
+  </div>
+  
+  <div className="flex items-center">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    version="1.1"
+    width="256"
+    height="256"
+    viewBox="0 0 256 256"
+    xmlSpace="preserve"
+    className="w-6 h-6 mr-2"
+  >
+    <defs></defs>
+    <g
+      style={{
+        stroke: "none",
+        strokeWidth: 0,
+        strokeDasharray: "none",
+        strokeLinecap: "butt",
+        strokeLinejoin: "miter",
+        strokeMiterlimit: 10,
+        fill: "none",
+        fillRule: "nonzero",
+        opacity: 1,
+      }}
+      transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)"
+    >
+      <path
+        d="M 79.35 77.115 H 10.651 C 4.778 77.115 0 72.337 0 66.464 V 23.536 c 0 -5.873 4.778 -10.65 10.65 -10.65 H 79.35 c 5.873 0 10.65 4.778 10.65 10.65 v 42.928 C 90 72.337 85.223 77.115 79.35 77.115 z M 10.65 19.885 c -2.013 0 -3.65 1.638 -3.65 3.65 v 42.928 c 0 2.014 1.638 3.651 3.651 3.651 H 79.35 c 2.013 0 3.65 -1.638 3.65 -3.651 V 23.536 c 0 -2.013 -1.638 -3.65 -3.65 -3.65 H 10.65 z"
+        style={{
+          stroke: "none",
+          strokeWidth: 1,
+          strokeDasharray: "none",
+          strokeLinecap: "butt",
+          strokeLinejoin: "miter",
+          strokeMiterlimit: 10,
+          fill: "rgb(0,0,0)",
+          fillRule: "nonzero",
+          opacity: 1,
+        }}
+        transform="matrix(1 0 0 1 0 0)"
+        strokeLinecap="round"
+      />
+      <path
+        d="M 45 60.229 c -0.796 0 -1.592 -0.271 -2.241 -0.812 l -30.71 -25.595 c -1.485 -1.237 -1.686 -3.444 -0.448 -4.93 c 1.237 -1.483 3.445 -1.686 4.929 -0.447 L 45 52.173 l 28.469 -23.727 c 1.484 -1.238 3.691 -1.036 4.93 0.447 c 1.237 1.485 1.037 3.692 -0.447 4.93 l -30.71 25.595 C 46.592 59.959 45.796 60.229 45 60.229 z"
+        style={{
+          stroke: "none",
+          strokeWidth: 1,
+          strokeDasharray: "none",
+          strokeLinecap: "butt",
+          strokeLinejoin: "miter",
+          strokeMiterlimit: 10,
+          fill: "rgb(0,0,0)",
+          fillRule: "nonzero",
+          opacity: 1,
+        }}
+        transform="matrix(1 0 0 1 0 0)"
+        strokeLinecap="round"
+      />
+    </g>
+  </svg>
+    <p className="text-medium md:text-xl lg:text-xl">info@kingscrossluggage.co.uk</p>
+  </div>
+  
+  <div className="flex items-center">
+  <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="24" height="24" viewBox="0 0 256 256" className="w-6 h-6 mr-2">
+    <defs></defs>
+    <g
+      style={{
+        stroke: 'none',
+        strokeWidth: 0,
+        strokeDasharray: 'none',
+        strokeLinecap: 'butt',
+        strokeLinejoin: 'miter',
+        strokeMiterlimit: 10,
+        fill: 'none',
+        fillRule: 'nonzero',
+        opacity: 1,
+      }}
+      transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)"
+    >
+      <path
+        d="M 63.063 0 H 26.937 c -2.816 0 -5.098 2.283 -5.098 5.098 v 79.803 c 0 2.816 2.283 5.098 5.098 5.098 h 36.126 c 2.816 0 5.098 -2.283 5.098 -5.098 V 5.098 C 68.161 2.283 65.879 0 63.063 0 z M 38.596 4.419 h 12.809 c 0.276 0 0.5 0.224 0.5 0.5 s -0.224 0.5 -0.5 0.5 H 38.596 c -0.276 0 -0.5 -0.224 -0.5 -0.5 S 38.319 4.419 38.596 4.419 z M 45 87.304 c -1.265 0 -2.291 -1.026 -2.291 -2.291 c 0 -1.265 1.026 -2.291 2.291 -2.291 s 2.291 1.026 2.291 2.291 C 47.291 86.278 46.265 87.304 45 87.304 z M 65.161 80 H 24.839 V 9.374 h 40.323 V 80 z"
+        style={{
+          stroke: 'none',
+          strokeWidth: 1,
+          strokeDasharray: 'none',
+          strokeLinecap: 'butt',
+          strokeLinejoin: 'miter',
+          strokeMiterlimit: 10,
+          fill: 'rgb(0,0,0)',
+          fillRule: 'nonzero',
+          opacity: 1,
+        }}
+        transform="matrix(1 0 0 1 0 0)"
+        strokeLinecap="round"
+      />
+    </g>
+  </svg>
+  <p className="text-lg md:text-xl lg:text-xl">075 3354 4428</p>
+</div>
+
+
+</div>
+
+
+
+ {/* nav bar */}
+<nav className="bg-green-800 w-full fixed top-[90px] md:top-10 left-0 right-0 z-30">
+  <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8 backdrop-blur-lg">
+    <div>
+      <div className="flex items-center justify-between py-3 md:py-5 md:block">
+        <Link href="/">
+          <h2 className="text-2xl text-white font-bold">Kings Cross Luggage</h2>
+        </Link>
+
+         
+              <div className="md:hidden">
+                <button
+                  className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                  onClick={() => setNavbar(!navbar)}
+                >
+                  {navbar ? (
+                    <Image src="/close.svg" width={30} height={30} alt="logo" />
+                  ) : (
+                    <Image
+                      src="/hamburger-menu.svg"
+                      width={30}
+                      height={30}
+                      alt="logo"
+                      className="focus:border-none active:border-none"
+                    />
+                  )}
+                </button>
+              </div>
+            </div>
+          </div>
+
+         
+          <div className="hidden md:flex items-center space-x-6 font-bold">
+            <ul className="flex space-x-14 text-white text-2xl">
+              <li className="hover:text-lime-400">
+                <Link href="/">Home</Link>
+              </li>
+              <li className="hover:text-lime-400">
+                <Link href="/about">About</Link>
+              </li>
+              <li className="hover:text-lime-400">
+                <Link href="#book">Book Now</Link>
+              </li>
+              <li className="hover:text-lime-400">
+                <Link href="#blog">Blog</Link>
+              </li>
+              <li className="hover:text-lime-400">
+                <Link href="#faqs">FAQs</Link>
+              </li>
+              <li className="hover:text-lime-400">
+                <Link href="#contact">Contact</Link>
+              </li>
+            </ul>
+          </div>
+
+        
+          <div className={`md:hidden ${navbar ? 'block' : 'hidden'} transition-all duration-300`}>
+            <ul className="flex flex-col items-center space-y-6 py-6  text-white text-xl h-screen top-0 right-0 left-0 font-bold">
+            <li className="hover:bg-lime-600 px-6 py-2 rounded-3xl">
+  <Link href="/" onClick={() => setNavbar(!navbar)}>Home</Link>
+</li>
+<li className="hover:bg-lime-600 px-6 py-2 rounded-3xl">
+  <Link href="#about" onClick={() => setNavbar(!navbar)}>About</Link>
+</li>
+<li className="hover:bg-lime-600 px-6 py-2 rounded-3xl">
+  <Link href="#book" onClick={() => setNavbar(!navbar)}>Book Now</Link>
+</li>
+<li className="hover:bg-lime-600 px-6 py-2 rounded-3xl">
+  <Link href="#blog" onClick={() => setNavbar(!navbar)}>Blog</Link>
+</li>
+<li className="hover:bg-lime-600 px-6 py-2 rounded-3xl">
+  <Link href="#faqs" onClick={() => setNavbar(!navbar)}>FAQs</Link>
+</li>
+<li className="hover:bg-lime-600 px-6 py-2 rounded-3xl">
+  <Link href="#contact" onClick={() => setNavbar(!navbar)}>Contact</Link>
+</li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
